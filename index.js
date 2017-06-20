c06b31e5ee6e8e5466cb2700eb9e870bac6e5c10
DavidTurkGT <dlturk747@gmail.com>
2017-06-20 10:17:06 -0400

    Refactored to utilize functions

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 const express = require("express");
 const app = express();
 
+function calculate(operation, num1, num2, res){
+  let ans;
+  switch(operation){
+    case "add":
+      ans = num1 + num2;
+      res.send("The sum of " + num1 + " and " + num2 + " is " + ans);
+      break;
+    case "subtract":
+      ans = num1 - num2;
+      res.send("The difference of " + num1 + " and " + num2 + " is " + ans);
+      break;
+    case "multiply":
+      ans = num1 * num2;
+      res.send("The product of " + num1 + " and " + num2 + " is " + ans);
+      break;
+    case "divide":
+      ans = num1 / num2;
+      res.send("The quotient of " + num1 + " and " + num2 + " is " + ans);
+      break;
+    case "pow":
+      ans = Math.pow(num1, num2);
+      res.send("The answer of " + num1 + " to the power of " + num2 + " is " + ans);
+      break;
+    default:
+      res.send("Invalid operation.  You sent " + operation + ".  This app only supports ADD, SUBTRACT, MULTIPLY, DIVIDE, or POW");
+      break;
+  }
+}
+
 app.get("/", function(req, res){
   res.send("Hello there!")
 });
@@ -12,32 +41,8 @@ app.get("/:operator/:num1/:num2", function(req, res){
 
       operation = operation.toLowerCase(); //standardize operation input
 
-      let ans;
-      switch(operation){
-        case "add":
-          ans = num1 + num2;
-          res.send("The sum of " + num1 + " and " + num2 + " is " + ans);
-          break;
-        case "subtract":
-          ans = num1 - num2;
-          res.send("The difference of " + num1 + " and " + num2 + " is " + ans);
-          break;
-        case "multiply":
-          ans = num1 * num2;
-          res.send("The product of " + num1 + " and " + num2 + " is " + ans);
-          break;
-        case "divide":
-          ans = num1 / num2;
-          res.send("The quotient of " + num1 + " and " + num2 + " is " + ans);
-          break;
-        case "pow":
-          ans = Math.pow(num1, num2);
-          res.send("The answer of " + num1 + " to the power of " + num2 + " is " + ans);
-          break;
-        default:
-          res.send("Invalid operation.  You sent " + operation + ".  This app only supports ADD, SUBTRACT, MULTIPLY, DIVIDE, or POW");
-          break;
-      }
+      calculate(operation, num1, num2, res);
+
 });
 
 app.listen(3000, function(){
